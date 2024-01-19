9138d00a7eadda4858c6611ed55d83b5200ca1b9
Leo Campos <leopcampos85@gmail.com>
2024-01-19 20:29:42 -0300

    Fazendo alterações no modelo de domínio e no classe de mapeamento para os testes funcionarem e criando o projeto que irá consumir os pedidos.

---
+++ src/Publisher/CentralDePedidos.Domain/Models/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using CentralDePedidos.Domain.Models.Enums;
+
+namespace CentralDePedidos.Domain.Models;
+
+public class Pedido
+{
+    public Guid? Id { get; set; }
+    public DateTime? DataHora { get; set; }
+    public decimal? Valor { get; set; }
+    public List<Produto>? Produtos { get; set; }
+    public StatusPedido? Status { get; set; }
+
+    public Guid? ClienteId { get; set; }
+    public Cliente? Cliente { get; set; }
+
+    public Guid? CobrancaId { get; set; }
+    public Cobranca? Cobranca { get; set; }
+}
